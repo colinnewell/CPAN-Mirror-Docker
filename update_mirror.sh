4bb2fa32aa372ff5fc4f13800f1916bd70ff9bb0
Colin Newell <c.newell@cv-library.co.uk>
2017-08-04 22:43:33 +0100

    Note where the files are stored

--- update_mirror.sh
+++ update_mirror.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 docker-compose run --rm cpan-mini /usr/local/bin/minicpan -d 0755
+echo Finished updating the mirror in $(docker inspect cpanmirrordocker_nginx_1 \
+    --format '{{ range .Mounts }}{{ .Source }}{{ end }}')
